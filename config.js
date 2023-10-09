9a023edb83a94e42d25c5a222f687cd396c1533c
di87654 <147370627+di87654@users.noreply.github.com>
2023-10-09 13:14:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: "dangbaongoc6a3",
-    introTitle: 'Tên người yêu bạn',
+    introTitle: 'Đặng Bảo Ngọc',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
